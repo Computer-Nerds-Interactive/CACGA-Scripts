af117b1688dc4af5dc70cf1d602d05fa275f5e04
MHinson0348 <madison@cni-inc.com>
2024-09-03 19:40:58 -0400

    p to %

--- home-slider-nav-adjust.js
+++ home-slider-nav-adjust.js
@@ -15,11 +15,11 @@ function handleWindowResize() {
 
         if (window.innerWidth <= 768) {
             console.log('Small screen detected.'); // Debugging statement
-            const newSlideLeftArrowPosition = (numberOfSlides * 20) + 60 + 'px';
-            const newSlideRightArrowPosition = (numberOfSlides * 20) + 62 + 'px';
+            const newSlideLeftArrowPosition = (numberOfSlides * 22) + 60 + 'px';
+            const newSlideRightArrowPosition = (numberOfSlides * 22) + 62 + 'px';
             slideLeftArrow.style.right = newSlideLeftArrowPosition;
             slideRightArrow.style.left = newSlideRightArrowPosition;
-            slideNav.style.width = 'auto';
+            slideNav.style.width = 100 + '%';
         } else {
             console.log('Large screen detected.'); // Debugging statement
             const newSlideNavWidth = (numberOfSlides * 22) + 118 + 'px';
