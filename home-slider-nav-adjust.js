e3211639d25fa97f5b67630cfe5023d62692f881
MHinson0348 <madison@cni-inc.com>
2024-09-03 19:30:26 -0400

    added window rezising adjustment

--- home-slider-nav-adjust.js
+++ home-slider-nav-adjust.js
@@ -25,7 +25,7 @@ function handleWindowResize() {
             const newSlideLeftArrowPosition = (numberOfSlides * 22) + 65 + 'px';
             slideNav.style.width = newSlideNavWidth;
             slideLeftArrow.style.right = newSlideLeftArrowPosition;
-            // No adjustment for slideRightArrow on larger screens
+            slideRightArrow.style.right = 18 + 'px';
         }
 
         // Log the number of slides and calculated values to the console
